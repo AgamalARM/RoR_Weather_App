0baf59e8d7162a7686a29f6d5b066f1af08cdf75
AgamalARM <agamalarm@gmail.com>
2023-07-04 10:43:53 +0000

    API Done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,22 @@
+require "net/http"
 class HomeController < ApplicationController
   def index
+    
+  end
+  
+  def show
+    
+  end
+  
+  def create
+    @zipcode = params[:zipcode]
+    @feeling = params[:feeling]
+    # @stops = Stop.where("longitude = ? AND latitude = ?", longitude, latitude)
+    url = "http://api.openweathermap.org/data/2.5/weather?zip=#{@zipcode}&appid=b51c99563a009490e658f79f448e8794&units=imperial"
+    uri = URI(url)
+    res = Net::HTTP.get_response(uri)
+    @data = JSON.parse(res.body)
+    render :index
   end
                      
 end
